8a79220f40792df6843e764a201e56e13b3aa6fb
shohrukhdev <shohrukhdev@gmail.com>
2025-01-22 09:55:24 +0500

    provided default DB_NAME

--- Blackstick/settings.py
+++ Blackstick/settings.py
@@ -106,7 +106,7 @@ WSGI_APPLICATION = 'Blackstick.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
+        "NAME": os.environ.get("DB_NAME", "postgres"),
         "USER": os.environ.get("DB_USER", "user"),
         "PASSWORD": os.environ.get("DB_PASSWORD", "password"),
         "HOST": os.environ.get("DB_HOST", "localhost"),
