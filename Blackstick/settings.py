128027f18f25a17407c69381dc3358e4548171d0
shohrukhdev <shohrukhdev@gmail.com>
2025-05-07 16:53:51 +0500

    to trigger the deploy

--- Blackstick/settings.py
+++ Blackstick/settings.py
@@ -264,7 +264,7 @@ LOCALE_PATHS = [os.path.join(BASE_DIR, "locale")]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/home'
