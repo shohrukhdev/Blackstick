6f0c841ee1ab7a2e530815f472b1f4279405f425
shohrukhdev <shohrukhdev@gmail.com>
2025-05-10 22:59:53 +0500

    test with old credentials

--- Blackstick/settings.py
+++ Blackstick/settings.py
@@ -209,8 +209,8 @@ USE_TZ = True
 
 if USE_S3 == "1":
     # aws settings
-    AWS_S3_ACCESS_KEY_ID = os.environ.get("AWS_S3_ACCESS_KEY_ID", "TEST_KEY")
-    AWS_S3_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_SECRET_ACCESS_KEY", "TEST")
+    # AWS_S3_ACCESS_KEY_ID = os.environ.get("AWS_S3_ACCESS_KEY_ID", "TEST_KEY")
+    # AWS_S3_SECRET_ACCESS_KEY = os.environ.get("AWS_S3_SECRET_ACCESS_KEY", "TEST")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_S3_STORAGE_BUCKET_NAME", "blackstickbucket")
     AWS_S3_DEFAULT_ACL = None
     AWS_LOCATION = "eu-north-1"
