e2774847f4a4b665af20bdd3be4ea4e6acd979c1
shohrukhdev <shohrukhdev@gmail.com>
2026-04-30 17:20:58 +0500

    Simplified static files collection by removing redundant ignored directories in production entrypoint script.

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -7,8 +7,7 @@ python manage.py migrate --no-input
 echo "==> Collecting static files..."
 python manage.py collectstatic --no-input \
   --ignore=chartjs --ignore=dist --ignore=dt --ignore=fc --ignore=jui \
-  --ignore=login --ignore=node_modules --ignore=plugins --ignore=summernote \
-  --ignore=admin --ignore=venv --ignore=rest_framework --ignore=fontawesomefree
+  --ignore=login --ignore=node_modules --ignore=plugins --ignore=summernote --ignore=venv
 
 echo "==> Compiling translations..."
 python manage.py compilemessages -l en
